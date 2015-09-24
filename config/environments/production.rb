cf21bf494f16088fd9ee0f649808309d99e0f992
rickymm3 <rickymm3@gmail.com>
2015-09-23 21:54:52 -0400

    asset data url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,4 @@ BaseApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.serve_static_files = true
 end
